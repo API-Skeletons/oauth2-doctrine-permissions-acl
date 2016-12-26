5ad3efc8f484bac0c1975f1dd13a586a8c15bdeb
Tom H Anderson <tom.h.anderson@gmail.com>
2016-12-25 20:01:35 -0700

    phpcs

--- src/Authorization/AclAuthorization.php
+++ src/Authorization/AclAuthorization.php
@@ -2,7 +2,8 @@
 /**
  * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
  * @copyright Copyright (c) 2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @copyright Copyright (c) 2016 Tom H Anderson <tom.h.anderson@gmail.com> for api-skeletons/zf-oauth2-doctrine-permissions
+ * @copyright Copyright (c) 2016 Tom H Anderson <tom.h.anderson@gmail.com>
+ *     for api-skeletons/zf-oauth2-doctrine-permissions
  */
 
 namespace ZF\OAuth2\Doctrine\Permissions\Acl\Authorization;
--- src/Role/ObjectRepositoryProvider.php
+++ src/Role/ObjectRepositoryProvider.php
@@ -4,7 +4,8 @@
  *
  * @link https://github.com/bjyoungblood/BjyAuthorize for the canonical source repository
  * @license http://framework.zend.com/license/new-bsd New BSD License
- * @copyright Copyright (c) 2016 Tom H Anderson <tom.h.anderson@gmail.com> for api-skeletons/zf-oauth2-doctrine-permissions
+ * @copyright Copyright (c) 2016 Tom H Anderson <tom.h.anderson@gmail.com>
+ *     for api-skeletons/zf-oauth2-doctrine-permissions
  */
 
 namespace ZF\OAuth2\Doctrine\Permissions\Acl\Role;
